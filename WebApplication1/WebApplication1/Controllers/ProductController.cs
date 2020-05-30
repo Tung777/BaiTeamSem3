32a472fa897637cc5d8aef5c56785abecbc8a40d
Tung777 <49684861+Tung777@users.noreply.github.com>
2020-05-30 14:25:11 +0700

    E:\GITWAD\BaiTeamSem3\project_sem_3-master\project_sem_3\Content\admin.css

---
+++ WebApplication1/WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        DbModelEntities1 db = new DbModelEntities1();
+        public ActionResult Index()
+        {
+            var listSP = db.Products;
+            return View(listSP);
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Create(string image,Product product)
+        {
+            if (image != null )
+            {
+                product.Image = string.Join(",", image);
+            }
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+           
+            return View(products);
+        }
+
+       
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit( Product products, string[] image)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(products).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           
+            return View(products);
+        }
+       
+
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product products = db.Products.Find(id);
+            if (products == null)
+            {
+                return HttpNotFound();
+            }
+            return View(products);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product products = db.Products.Find(id);
+            db.Products.Remove(products);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
