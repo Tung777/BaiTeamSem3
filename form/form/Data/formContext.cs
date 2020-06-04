6f95eb2b03ec27b153fa043b2d5263f750e11813
Tung777 <49684861+Tung777@users.noreply.github.com>
2020-06-04 19:54:57 +0700

    E:\GITWAD\BaiTeamSem3\form\form\App_Start\RouteConfig.cs
    Validation form

---
+++ form/form/Data/formContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace form.Data
+{
+    public class formContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public formContext() : base("name=formContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<form.Models.Product> Products { get; set; }
+    }
+}
---
+++ form/form/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace form.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        //Độ dài max là 60,nhỏ là 3
+        [Required, StringLength(60, MinimumLength = 3)]
+        public string Ten { get; set; }
+        //bắt buộc phải ghi vào độ dài tối thiểu là 5
+        [Required, StringLength(5)]
+        public string NhaCungCap { get; set; }
+        //số lượng trong khoảng 1-100
+        [Range(1, 100), DataType(DataType.Currency)]
+        public int Soluong { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime NgayNhap { get; set; }
+        [Range(1, 100), DataType(DataType.Currency)]
+        public double Gia { get; set; }
+ 
+
+    }
+    }
